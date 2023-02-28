34b22bbf91b168ecb57600853aa51359853898ce
lauren36 <111887636+lauren36@users.noreply.github.com>
2023-02-28 08:19:06 -0600

    Initial commit

---
+++ Casting_Omalley_Lauren/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Casting_Omalley_Lauren
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Casting_Omalley_Lauren
+ 
