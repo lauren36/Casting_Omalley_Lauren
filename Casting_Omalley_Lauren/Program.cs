4685e87c87d46b1e0a95c0813918d14226b8ddac
lauren36 <111887636+lauren36@users.noreply.github.com>
2023-02-28 08:44:14 -0600

    plus

--- Casting_Omalley_Lauren/Program.cs
+++ Casting_Omalley_Lauren/Program.cs
@@ -5,8 +5,24 @@ namespace Casting_Omalley_Lauren
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+
+        {   // Asks the user to enter a whole number.
+            Console.WriteLine("Please enter a whole number!");
+
+            // Assigns the input to the integer.
+            int myFavNum = Convert.ToInt32(Console.ReadLine());
+
+            // Shows the number you typed in after Your favorite number is.
+            Console.WriteLine("Your favorite number is " + myFavNum);
+
+            // Asks the user if they like video games in a true or false situation.
+            Console.WriteLine("Do you like video games? (Please enter 'true' or 'false')");
+
+            // Assigns the response to a boolean.
+            bool isTrue = Convert.ToBoolean(Console.ReadLine());
+
+            // Shows the result of the true or false question.
+            Console.WriteLine($"It is {isTrue} that I like video games!");
         }
     }
 }
